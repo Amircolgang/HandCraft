2c028190660fe0673f853e141cced2ead9f2e1d5
amircolgang <parham38385747@gmail.com>
2025-07-02 15:23:19 +0330

    Amircolgang: changes on 2025-07-02 15:23:18

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Page (params) {
   return(
     <>
-      <div className="container mx-auto">
+      <div className="container mx-auto px-[]">
         <h1>
           Salm
         </h1>
